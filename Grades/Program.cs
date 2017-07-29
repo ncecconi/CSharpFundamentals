dd8d00f86a515d585092472bf869f3c027578085
ncecconi <nick.cecconi@gmail.com>
2017-07-29 08:00:03 -0500

    Add project files.

---
+++ Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                         
+            GradeBook book = new GradeBook();
+            book.Name = "Nick's Grade Book";
+            book.Name = null;
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            Console.ReadLine();
+        }
+    }
+}
